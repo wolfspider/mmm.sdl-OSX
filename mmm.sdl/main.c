0662e8b34c2b8453e345794dd20276606f4d99d5
Jesse Bennett <jesse.alexander.bennett@gmail.com>
2017-09-25 14:42:38 -0400

    Initial Commit

---
+++ mmm.sdl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mmm.sdl
+//
+//  Created by Jesse Bennett on 9/25/17.
+//  Copyright © 2017 Jesse Bennett. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
